ba612b68d080b3da26fb3084c302affd4dd3dfaa
harsh-panchal-804 <harshpanchal522@gmail.com>
2025-01-08 15:13:47 +0530

    Z Algorithm

---
+++ Finding_Borders.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+const long long mod = 1e9 + 7;
+#define INF (1LL << 60)
+#define inf (1 << 30)
+#define all(x) (x).begin(), (x).end()
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+
+template <class T, class V> void _print(pair <T, V> p) {
+    cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";
+}
+template <class T> void _print(vector <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(set <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(multiset <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T, class V> void _print(map <T, V> v) {
+    cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+vector<int> zfunction(string & s){
+    int n=s.size();
+    vector<int> z(n);
+    z[0]=n;
+    int l=0;
+    int r=0;
+    for(int i=1;i<n;i++){
+        if(i<r){
+            z[i]=min(r-i,z[i-l]);
+        }
+        while(i+z[i]<n && s[z[i]]==s[i+z[i]])z[i]++;
+        if(i+z[i]>r){
+            l=i;
+            r=i+z[i];
+        }
+    }
+    return z;
+
+}
+vector<int> ans;
+
+
+void solve(){
+    string s;
+    cin>>s;
+    int n=s.size();
+
+    ans.resize(n+1,0);
+    vector<int> z=zfunction(s);
+    ///// z[i]==z[n-i] n=10 //// i+z[i]==n
+    //////// a b c a b a b c a b
+    /// z [ 10 0 0 2 0 5 0 0 2 0 ]
+    for(int i=0;i<n;i++){
+        if(i+z[i]==n){
+            ans[z[i]]=1;
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(ans[i] && i!=n){
+            cout<<i<<" ";
+        }
+    }
+    cout<<"\n";
+    
+
+
+    
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt", "w", stderr);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    int test=1;
+    // cin>>test;
+    while(test--){
+         solve();
+    }
+    
+    return 0;
+}
