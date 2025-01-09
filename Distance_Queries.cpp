893be5e6ea1ef0d4727afe69962012c309916299
harsh-panchal-804 <harshpanchal522@gmail.com>
2025-01-09 12:07:23 +0530

    Binary Lifting LCA

--- Distance_Queries.cpp
+++ Distance_Queries.cpp
@@ -7,13 +7,13 @@ const long long mod = 1e9 + 7;
 #define INF (1LL << 60)
 #define inf (1 << 30)
 #define all(x) (x).begin(), (x).end()
-
+ 
 #ifndef ONLINE_JUDGE
 #define debug(x) cerr << #x << " "; _print(x); cerr << endl;
 #else
 #define debug(x)
 #endif
-
+ 
 void _print(ll t) { cerr << t; }
 void _print(int t) { cerr << t; }
 void _print(string t) { cerr << t; }
@@ -21,14 +21,14 @@ void _print(char t) { cerr << t; }
 void _print(lld t) { cerr << t; }
 void _print(double t) { cerr << t; }
 void _print(ull t) { cerr << t; }
-
+ 
 template <class T, class V> void _print(pair <T, V> p);
 template <class T> void _print(vector <T> v);
 template <class T> void _print(set <T> v);
-
+ 
 template <class T, class V> void _print(map <T, V> v);
 template <class T> void _print(multiset <T> v);
-
+ 
 template <class T, class V> void _print(pair <T, V> p) {
     cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";
 }
@@ -61,9 +61,9 @@ void dfs2(int node,int prev=-1){
         dfs2(child,node);
     }
 }
-
-
-
+ 
+ 
+ 
 void solve(){
     int n,q;
     cin>>n>>q;
@@ -117,11 +117,11 @@ void solve(){
         int depth_lca=depth[lca];
         cout<<depth[x] +depth[y] -2*depth_lca<<"\n";
     }
-
-
+ 
+ 
     
 }
-
+ 
 int main() {
     #ifndef ONLINE_JUDGE
         freopen("Error.txt", "w", stderr);
