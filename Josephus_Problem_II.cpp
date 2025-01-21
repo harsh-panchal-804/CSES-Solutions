e6b7e3db22edd7c3881cc2e4fd5f7d30a8806301
harsh-panchal-804 <harshpanchal522@gmail.com>
2025-01-21 19:56:13 +0530

    Ordered Set PBDS

---
+++ Josephus_Problem_II.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+const long long mod = 1e9 + 7;
+#define INF (1LL << 60)
+#define inf (1 << 30)
+#define all(x) (x).begin(), (x).end()
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+
+template <class T, class V> void _print(pair <T, V> p) {
+    cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";
+}
+template <class T> void _print(vector <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(set <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(multiset <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T, class V> void _print(map <T, V> v) {
+    cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+template<class T> 
+using oset=tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+
+
+
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    oset<int> set;
+    for(int i=1;i<=n;i++){
+        set.insert(i);
+    }
+    int start=0;
+    while(set.size()){
+        start%=set.size();
+        int remove=(start +k)%set.size();
+        auto ele=set.find_by_order(remove);
+        cout<<*ele<<' ';
+        set.erase(ele);
+        start=remove;
+    }
+    cout<<"\n";
+    
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt", "w", stderr);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    int test=1;
+    // cin>>test;
+    while(test--){
+         solve();
+    }
+    
+    return 0;
+}
