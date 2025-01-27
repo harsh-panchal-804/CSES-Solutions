06bbc5f203deb1a1dfcb3776c4d84c148db785be
harsh-panchal-804 <harshpanchal522@gmail.com>
2025-01-27 10:02:51 +0530

    Inclusion-Exclusion with Bitmasks

---
+++ Prime_Multiples.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+const long long mod = 1e9 + 7;
+#define INF (1LL << 60)
+#define inf (1 << 30)
+#define all(x) (x).begin(), (x).end()
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+
+template <class T, class V> void _print(pair <T, V> p) {
+    cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";
+}
+template <class T> void _print(vector <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(set <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(multiset <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T, class V> void _print(map <T, V> v) {
+    cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+
+
+
+void solve(){
+    ll  n,k;
+    cin>>n>>k;
+    vector<ll > vec(k);
+    for(ll  i=0;i<k;i++)cin>>vec[i];
+    ll  ans=0;
+    for(ll  mask=1;mask<(1<<k);mask++){
+        ll  temp=n;
+        int cnt=0;
+        for(ll  i=k;i>=0;i--){
+            if((1<<i) &  mask){
+                temp/=vec[i];
+            }
+        }
+        if(__builtin_popcount(mask) & 1){
+            ans += temp;
+        }
+        else{
+            ans -= temp;
+        }
+    }
+    cout<<ans<<"\n";
+    
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt", "w", stderr);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    int test=1;
+    // cin>>test;
+    while(test--){
+         solve();
+    }
+    
+    return 0;
+}
