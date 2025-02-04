6e9c3ad933eb0fd2c34ae974d6ad5c02ccf5ee6f
harsh-panchal-804 <harshpanchal522@gmail.com>
2025-02-04 11:24:28 +0530

    Bipartite Graph Checking

---
+++ Building_Teams.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+const long long mod = 1e9 + 7;
+#define INF (1LL << 60)
+#define inf (1 << 30)
+#define all(x) (x).begin(), (x).end()
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+
+template <class T, class V> void _print(pair <T, V> p) {
+    cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";
+}
+template <class T> void _print(vector <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(set <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T> void _print(multiset <T> v) {
+    cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+template <class T, class V> void _print(map <T, V> v) {
+    cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";
+}
+
+
+
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    vector<int> graph[n+1];
+    while(m--){
+        int a,b;
+        cin>>a>>b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+    vector<int> colors(n+1,-1);
+    function<bool(int,int)> dfs=[&](int node,int c){
+        colors[node]=c;
+        for(auto child: graph[node]){
+            if(colors[child]==-1){
+                if(!dfs(child,c^1^2))return false;
+            }
+            else if(colors[child] ==c){
+                return false;
+            }
+        }
+        return true;
+        
+    };
+    for(int i=1;i<=n;i++){
+        if(colors[i]==-1){
+            if(!dfs(i,1)){
+                cout<<"IMPOSSIBLE"<<"\n";
+                return;
+            };
+        }
+    }
+
+    for(int i=1;i<=n;i++){
+        cout<<colors[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt", "w", stderr);
+    #endif
+    ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+    int test=1;
+    // cin>>test;
+    while(test--){
+         solve();
+    }
+    
+    return 0;
+}
