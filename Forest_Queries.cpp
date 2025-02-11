b6d05ffefa4e8c98b3bbc1ab245fd6d0e596d0e2
harsh-panchal-804 <harshpanchal522@gmail.com>
2025-02-11 10:21:16 +0530

    Prefix Sum

--- Forest_Queries.cpp
+++ Forest_Queries.cpp
@@ -70,9 +70,9 @@ void solve(){
         int y1,x1,y2,x2;
         cin>>x1>>y1>>x2>>y2;
         int result = dp[x2][y2]
-                   - (x1 > 1 ? dp[x1-1][y2] : 0)
-                   - (y1 > 1 ? dp[x2][y1-1] : 0)
-                   + (x1 > 1 && y1 > 1 ? dp[x1-1][y1-1] : 0);
+                   -  dp[x1-1][y2] 
+                   - dp[x2][y1-1] 
+                   +dp[x1-1][y1-1];
         cout << result << endl;
     }
 
